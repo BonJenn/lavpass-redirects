5dee4d52ae0cd839db0dcc99b041ee04d2fd580a
Jonathan Benn <bonjennprojects@gmail.com>
2025-05-12 11:30:28 -0700

    Add social media redirects

---
+++ src/app/ig/page.tsx
@@ -0,0 +1,6 @@
+// src/app/instagram/page.tsx
+import { redirect } from 'next/navigation';
+
+export default function InstagramRedirect() {
+  redirect('https://apps.apple.com/app/id6744234737');
+}
---
+++ src/app/tt/page.tsx
@@ -0,0 +1,5 @@
+import { redirect } from 'next/navigation';
+
+export default function TikTokRedirect() {
+  redirect('https://apps.apple.com/app/id6744234737');
+}
